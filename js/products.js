e5b42abf074faf8f241a19365a3ad84445c1fbfa
Gonzalo di Gesu <gonzalo.digesu@nimacloud.com>
2023-03-13 19:43:32 -0300

    changes

---
+++ js/products.js
@@ -0,0 +1,54 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Figura Goku",
+        precio: 5700,
+        img:"img/figura_goku.jpg",
+        cantidad: 1,
+        tamaño: "25cm",
+        material:"PVC",
+        origen:"China"
+
+    },
+    {
+        id: 2,
+        nombre: "Figura Baji",
+        precio: 4300,
+        img:"img/figura_baji.jpg",
+        cantidad: 1,
+        tamaño: "22cm",
+        material:"PVC",
+        origen:"China"
+    },
+    {
+        id: 3,
+        nombre: "Buzo Kimetsu",
+        precio: 5600,
+        img:"img/buzo_kimetsu_no_yaiba.jpg",
+        cantidad: 1,
+        tamaño: "M",
+        material:"Algodon",
+        origen:"China"
+    },
+    
+    {
+        id: 4,
+        nombre: "Figura All Might",
+        precio: 9600,
+        img:"img/figura_all_might.jpg",
+        cantidad: 1,
+        tamaño: "30cm",
+        material:"PVC",
+        origen:"Japon"
+    },
+    {
+        id: 5,
+        nombre: "Figura Itachi",
+        precio: 3900,
+        img:"img/figura_itachi.jpg",
+        cantidad: 1,
+        tamaño: "20cm",
+        material:"PVC",
+        origen:"China"
+      },
+  ];
